fcfc37934ca42ecd768b455a836bd1fa47f56f74
‘lizhenhui <lizhenhui@csiisz.com>
2020-12-29 09:42:22 +0800

    storege优化

--- storge/index.js
+++ storge/index.js
@@ -1,46 +1,35 @@
-
-const cacheMap = {
+const map = {
     0: localStorage,
     1: sessionStorage
-};
-
-const isObject = (value) => (typeof value === 'object' && value !== null)
-
-const getValue = (key, type) => {
-    let value = cacheMap[type][key];
-    if (isObject(value)) {
-        return JSON.stringify(value);
+  };
+  
+  const getValue = (key, type) => {
+    const value = map[type][key];
+    return value ? JSON.parse(value) : undefined;
+  };
+  
+  const setValue = (key, value, type) => {
+    // 如果需要删除 则直接设置值为undefined即可
+    if (undefined === value) {
+      map[type].removeItem(key);
     } else {
-        return value
-    }
-};
-
-const setValue = (key, value, type) => {
-    if ([undefined, ''].includes(value)) {
-        cacheMap[type].removeItem(key);
-    } else {
-        if (isObject(value)) {
-            cacheMap[type][key] = JSON.stringify(value);
-        } else {
-            cacheMap[type][key] = value;
-        }
+      map[type][key] = JSON.stringify(value)
     }
     return true;
-};
-
-const returnProxy = type => {
-    return new Proxy(
-        {},
-        {
-            get (target, key) {
-                return getValue(key, type);
-            },
-            set (target, key, value) {
-                return setValue(key, value, type);
-            }
-        }
+  };
+  
+  const getProxy = (type, StorageData) => {
+    return new Proxy(StorageData, {
+      get (target, key) {
+        return getValue(key, type);
+      },
+      set (target, key, value) {
+        return setValue(key, value, type);
+      }
+    }
     );
-};
-
-// Storage工具类
-const [setLocal, setSession] = [returnProxy(0), returnProxy(1)];
+  };
+  
+  // Storage工具类
+  const [setLocal, setSession] = [getProxy(0, {}), getProxy(1, {})];
+    
