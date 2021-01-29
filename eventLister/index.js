8b05f79c5fc24708e379842f352e4ab2cd558dae
‘lizhenhui <lizhenhui@csiisz.com>
2021-01-29 09:21:16 +0800

    优化eventlister

--- eventLister/index.js
+++ eventLister/index.js
@@ -11,8 +11,10 @@ class EventLister {
         }
     }
     emit (eventName, ...args) {
-        const item = this.handler[eventName]
-        item && item.forEach(callback => callback(args))
+        Promise.resolve().then(() => {
+            const item = this.handler[eventName]
+            item && item.forEach(callback => callback(args))
+        })
     }
     unResgiterEvent (eventName) {
         if (this.handler[eventName]) {
